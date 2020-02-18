6c2f343ebca82b087576b437645aabc3ac55a4b9
Shengyi Qian <syqian@umich.edu>
2020-02-18 02:55:32 -0500

    only detect gpu jobs

--- nicebot.py
+++ nicebot.py
@@ -1,16 +1,108 @@
 from slacker import Slacker
 import psutil
-import socket
+import sys
 import os
+import re
+import subprocess
+import select
+
+def collect_gpu_info():
+    MEMORY_FREE_RATIO = 0.05
+    MEMORY_MODERATE_RATIO = 0.9
+    GPU_FREE_RATIO = 0.05
+    GPU_MODERATE_RATIO = 0.75
+
+    #parser = argparse.ArgumentParser()
+    #parser.add_argument('-l', '--command-length', default=20, const=100, type=int, nargs='?')
+    #parser.add_argument('-c', '--color', action='store_true')
+
+    #args = parser.parse_args()
+
+    # parse the command length argument
+    command_length = 20
+    color = True
+
+    # for testing, the stdin can be provided in a file
+    fake_stdin_path = os.getenv("FAKE_STDIN_PATH", None)
+    stdin_lines = []
+    if sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
+        stdin_lines = sys.stdin.readlines()
+
+    if fake_stdin_path is not None:
+        with open(fake_stdin_path, 'rt') as f:
+            lines = f.readlines()
+    elif stdin_lines:
+        lines = stdin_lines
+    else:
+        processes = subprocess.run('nvidia-smi', stdout=subprocess.PIPE)
+        lines_proc = processes.stdout.decode().split("\n")
+        lines = [line + '\n' for line in  lines_proc[:-1]]
+        lines += lines_proc[-1]
+
+    lines_to_print = []
+    # Copy the utilization upper part verbatim
+    for i in range(len(lines)):
+        if not lines[i].startswith("| Processes:"):
+            lines_to_print.append(lines[i].rstrip())
+        else:
+            i += 3
+            break
+
+    #for line in lines_to_print:
+    #    print(line)
+
+    # Parse the PIDs from the lower part
+    gpu_num = []
+    pid = []
+    gpu_mem = []
+    user = []
+    cpu = []
+    mem = []
+    time = []
+    command = []
+
+    no_running_process = "No running processes found"
+    if no_running_process in lines[i]:
+        return gpu_num, pid, user, gpu_mem, cpu, mem, time, command
+
+    while not lines[i].startswith("+--"):
+        if "Not Supported" in lines[i]:
+            i+=1
+            continue
+        line = lines[i]
+        line = re.split(r'\s+', line)
+        gpu_num.append(line[1])
+        pid.append(line[2])
+        gpu_mem.append(line[-3])
+        user.append("")
+        cpu.append("")
+        mem.append("")
+        time.append("")
+        command.append("")
+        i+=1
+
+    # Query the PIDs using ps
+    ps_format = "pid,user,%cpu,%mem,etime,command"
+    processes = subprocess.run(["ps", "-o", ps_format, "-p", ",".join(pid)], stdout=subprocess.PIPE)
+
+    # Parse ps output
+    for line in processes.stdout.decode().split("\n"):
+        if line.strip().startswith("PID") or len(line) == 0:
+            continue
+        parts = re.split(r'\s+', line.strip(), 5)
+        idx = pid.index(parts[0])
+        user[idx] = parts[1]
+        cpu[idx] = parts[2]
+        mem[idx] = parts[3]
+        time[idx] = parts[4] if not "-" in parts[4] else parts[4].split("-")[0] + " days"
+        command[idx] = parts[5][0:100]
+
+    return gpu_num, pid, user, gpu_mem, cpu, mem, time, command
 
 
-def main():
-    token = os.environ['NICEBOT_TOKEN']
-    slack = Slacker(token)
+def collect_cpu_info():
     procs = psutil.process_iter(attrs=['pid', 'name', 'username','nice'])
-    hostname = socket.gethostname()
-    prefix = "Performing nice scan on {}: ".format(hostname)
-    message = prefix + '\n```'
+    message = '```'
     cnt = 0
     for p in procs:
         info = p.info
@@ -21,12 +113,35 @@ def main():
         if nice < 10:
             message += str(info) + '\n'
             cnt += 1
-
+    # Send a message to #general channel
     message += '```'
+
+
+def main():
+    slack = Slacker('xoxp-498725714531-500356459845-797596978710-9544d3f3dfd64f2cb8a1149e4d70a496')
+    
+    # collect gpu info
+    message = '```'
+    cnt = 0
+    command_length = 20
+    gpu_num, pid, user, gpu_mem, cpu, mem, time, command = collect_gpu_info()
+    format = ("|  %3s %5s %8s   %8s %5s %5s %9s  %-" + str(command_length) + "." + str(command_length) + "s  |")
+    message += format % ("GPU", "PID", "USER", "GPU MEM", "%CPU", "%MEM", "TIME", "COMMAND")
+    message += '\n'
+    for i in range(len(pid)):
+        proc = psutil.Process(pid=int(pid[i]))
+        nice = proc.nice()
+        if nice < 10:
+            cnt += 1
+            message += format % (gpu_num[i], pid[i], user[i], gpu_mem[i],
+                cpu[i], mem[i], time[i], command[i])
+            message += '\n'
+    message += '```'
+
     if cnt > 0:
         slack.chat.post_message('#nicebot', message)
     else:
-        slack.chat.post_message('#nicebot', prefix + "very nice!")
+        slack.chat.post_message('#nicebot', "very nice in this scan!")
 
 
 if __name__=='__main__':
